af1b497d7cdf3699595b546e9a223b8dc7ef11d5
mari-barbosa <marimbbarbosa93@gmail.com>
2025-04-26 12:56:52 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask,render_template, request, Response
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from time import sleep
+
+load_dotenv()
+
+CHAVE_API_GOOGLE = os.getenv("GEMINI_API_KEY")
+MODELO_ESCOLHIDO = "gemini-2.5-pro-exp-03-25"   
+genai.configure(api_key=CHAVE_API_GOOGLE)
+
+app = Flask(__name__)
+app.secret_key = 'furia'
+
+def bot(prompt):
+    maximo_tentativas = 1
+    repeticao = 0
+
+    while True:
+        try:
+            prompt_do_sistema = f"""
+            Você é um chatbot de atendimento a fãs da Furia CS. 
+            Você não deve responder perguntas que não sejam dados sobre a Furia CS!
+            """
+
+            configuracao_modelo = {
+                "temperature" : 0.1,
+                "max_output_tokens" : 8192
+            }
+
+            llm = genai.GenerativeModel(
+                model_name=MODELO_ESCOLHIDO,
+                system_instruction=prompt_do_sistema,
+                generation_config=configuracao_modelo
+            )
+
+            resposta = llm.generate_content(prompt)
+            return resposta.text
+        except Exception as erro:
+            repeticao += 1
+            if repeticao >= maximo_tentativas:
+                return "Erro no Gemini: %s" % erro
+            
+            sleep(50)
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["msg"]
+    resposta = bot(prompt)
+    return resposta
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
